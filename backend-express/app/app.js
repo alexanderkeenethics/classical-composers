65c8d3685f24d011a30d5d5464d64690b2b56926
alexanderkeenethics <alexander.zarudnyak@keenethics.com>
2024-09-12 17:36:27 +0200

    update backend: - add endpoint for fetching all composers; - add endpoint for fetching contact data by composer id;

--- backend-express/app/app.js
+++ backend-express/app/app.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 require('dotenv-flow').config();
 
 const indexRouter = require('./routes/index');
+const {getAllComposers, getContactData} = require("./utils/db-utils");
+const console = require("node:console");
 
 const app = express();
 const LOG = require('./logger')(module);
@@ -21,13 +23,36 @@ LOG.info('**** Classical Composers Service ****');
 
 
 const corsOpts = {
-	origin: [],
+	origin: ['http://localhost:3000'],
 	optionsSuccessStatus: 200
 };
 
 app.use( cors( corsOpts ) );
 app.use( '/', indexRouter);
+app.get('/composers-list', async (req, res, next) => {
+	try {
+		const data = await getAllComposers();
+		if (!data) throw new Error('Internal Server Error')
 
+		res.status(200).json(data);
+	} catch (error) {
+		next(error)
+	}
+})
+
+app.get('/composer-contact-data/:composerId', async (req, res, next) => {
+	try {
+		console.log('enter --> composer-contact-data');
+		const composerId = req.params.composerId;
+		console.log('composerId -->', composerId);
+		const contactData = await getContactData(composerId);
+		console.log('getComposerContactData -->', contactData);
+		if (!contactData) throw new Error('Internal Server Error');
+		res.status(200).json(contactData);
+	} catch (error) {
+		next(error)
+	}
+})
 // Error handler should be last
 app.use(function errorHandler(err, req, res, next) {
 	if ( !err.logged ) LOG.error(err);
